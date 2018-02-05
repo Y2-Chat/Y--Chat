2d3c68959471b89881d8e63b14c5725e5fbfc9a5
Dewald <moutondewald@gmail.com>
2018-02-05 11:49:35 +0200

    Implemented google login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = []
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'userProfile',
+        pathMatch: 'full'
+    }, {
+        path: 'userProfile',
+        component: RegisterComponent
+    }]
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
