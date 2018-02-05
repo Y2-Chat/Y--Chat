e18e38211313399a68bf5a1d87811782a5714ba8
Keenan Idas <k9idas@gmail.com>
2018-02-05 08:41:47 +0200

    created initial objects
    created initial message, user and chat objects

---
+++ src/app/models/chat.model.ts
@@ -0,0 +1,6 @@
+import { Message } from './message.model';
+export class Chat{
+    chatId:string;
+    messages:Message[];
+    users:string[];
+}
---
+++ src/app/models/message.model.ts
@@ -0,0 +1,7 @@
+export class Message{
+    messageId:string;
+    body:string;
+    senderUid:string;
+    read:boolean;
+    time:Date;
+}
